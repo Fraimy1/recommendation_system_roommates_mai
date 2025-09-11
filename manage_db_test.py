cb5bdcb57fee442458b6de3a674f39915e1f96da
Fraimy1 <a.baxtiozinn@gmail.com>
2025-09-11 09:08:44 +0300

    test neo4j server

---
+++ manage_db_test.py
@@ -0,0 +1,32 @@
+from neo4j import GraphDatabase, RoutingControl, bearer_auth
+
+
+URI = "neo4j+s://b1c56ad1.databases.neo4j.io"
+
+auth = ("neo4j", "5eGdrGA1DfOqSZk_TdM1p9R_Mq02lgLrtpLCQbtvYWo")
+
+
+def add_friend(driver, name, friend_name):
+    driver.execute_query(
+        "MERGE (a:Person {name: $name}) "
+        "MERGE (friend:Person {name: $friend_name}) "
+        "MERGE (a)-[:KNOWS]->(friend)",
+        name=name, friend_name=friend_name, database_="neo4j",
+    )
+
+
+def print_friends(driver, name):
+    records, _, _ = driver.execute_query(
+        "MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+        "RETURN friend.name ORDER BY friend.name",
+        name=name, database_="neo4j", routing_=RoutingControl.READ,
+    )
+    for record in records:
+        print(record["friend.name"])
+
+
+with GraphDatabase.driver(URI, auth=auth) as driver:
+    add_friend(driver, "Arthur", "Guinevere")
+    add_friend(driver, "Arthur", "Lancelot")
+    add_friend(driver, "Arthur", "Merlin")
+    print_friends(driver, "Arthur")
